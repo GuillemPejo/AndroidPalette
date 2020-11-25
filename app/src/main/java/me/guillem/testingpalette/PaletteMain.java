2d353bdc01d6b960158825d7b1375ce207ed328a
guillem <guillempejo@gmail.com>
2020-11-25 23:17:36 +0100

    Add a recyclerview and card colors views

--- app/src/main/java/me/guillem/testingpalette/PaletteMain.java
+++ app/src/main/java/me/guillem/testingpalette/PaletteMain.java
@@ -5,12 +5,19 @@ import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.view.ContextThemeWrapper;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.palette.graphics.Palette;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 /**
  * * Created by Guillem on 21/11/20.
  */
@@ -38,9 +45,6 @@ public class PaletteMain extends AppCompatActivity {
         textViewTitle = findViewById(R.id.textTitle);
         textViewBody = findViewById(R.id.textBody);
         imageView = findViewById(R.id.imageView);
-        Bitmap bitmap = null;
-
-        imageFrom(imageView);
 
         imageView.getLayoutParams().height = 250;
         imageView.getLayoutParams().width = 250;
@@ -48,8 +52,13 @@ public class PaletteMain extends AppCompatActivity {
 
         LinearLayout.LayoutParams parms = new LinearLayout.LayoutParams(250,250);
         imageView.setLayoutParams(parms);
-        //Bitmap bitmap = ((BitmapDrawable) imageView.getDrawable()).getBitmap();
-        Palette.from(bitmap).maximumColorCount(32).generate(new Palette.PaletteAsyncListener() {
+
+        imageView.setImageResource(R.drawable.image);
+        Bitmap bitmap = ((BitmapDrawable) imageView.getDrawable()).getBitmap();
+
+        Palette.Builder from = Palette.from(bitmap);
+        from.maximumColorCount(32);
+        from.generate(new Palette.PaletteAsyncListener() {
 
             @Override
             public void onGenerated(Palette palette) {
@@ -60,24 +69,84 @@ public class PaletteMain extends AppCompatActivity {
                 lightMutedSwatch = palette.getLightMutedSwatch();
                 darkMutedSwatch = palette.getDarkMutedSwatch();
 
-                textViewBody.setText(vibrantSwatch.toString());
+                List<Palette.Swatch> pss;
+                List<List<String>> poo = null;
+                LinearLayout linearLayout = new LinearLayout(PaletteMain.this);
+                setContentView(linearLayout);
+                linearLayout.setOrientation(LinearLayout.VERTICAL);
+                ;
+                pss = palette.getSwatches();
+                for (int j = 0; j < 5; j++) {
+                    Palette.Swatch ps = pss.get(j);
+                    System.out.println(ps.toString());
 
 
-            }
+                    //CardView card = new CardView(PaletteMain.this);
+                    //LinearLayout cardInner = new LinearLayout(PaletteMain.this);
+
+                    TextView tv_title = new TextView(PaletteMain.this);
+                    tv_title.setLayoutParams(new LinearLayout.LayoutParams(
+                            LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT
+                    ));
+                    tv_title.setText("Vibrant");
+
+                    TextView tv_caption = new TextView(PaletteMain.this);
+                    tv_caption.setLayoutParams(new LinearLayout.LayoutParams(
+                            LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT
+                    ));
+                    tv_caption.setText("RGB: "+ps.getRgb()+" HSL: "+ps.getHsl()+" Population: "+ps.getPopulation()+" TitleText: "+ps.getTitleTextColor()+" BodyText: " +ps.getBodyTextColor());
+
+                    linearLayout.addView(tv_title);
+                    linearLayout.addView(tv_caption);
+
+
+
+
+
+                }
+
+
+/*
+                System.out.println("LISTA");
+                System.out.println("-------------------------");
+                for (int i = 0; i <= poo.get(0).size() - 1; i++) {
+                    System.out.println("Nom: " + poo.get(i).get(0) + " Color: "
+                            + poo.get(i).get(1) + " Population: " + poo.get(i).get(2)
+                            + " HSL: " + poo.get(i).get(3) + " BodyTextColor: " + poo.get(i).get(4) + " TitleTextColor: " + poo.get(i).get(4)
+                    );
+                    System.out.println("-------------------------");
+                    }
+*/
+
+
+
+/*                for(Palette.Swatch model : pss) {
+                    String color = "#" + Integer.toHexString(model.getRgb());
+                    //textViewBody.setText("Color: " + color);
+                    //textViewBody.setTextColor((Color.parseColor(color)));
+                    System.out.println(color);
+
+                }*/
+
+
+        }
         });
     }
 
-    public void imageFrom(ImageView imageView){
+    public Bitmap imageFrom(ImageView imageView){
         Bitmap bitmap;
         if(getIntent()!=null){
             Intent intent = getIntent();
             bitmap = ((Bitmap) intent.getParcelableExtra("BitmapImage"));
-            imageView.setImageBitmap(bitmap);}
+            //bitmap = ((BitmapDrawable) imageView.getDrawable()).getBitmap();
+        return bitmap;
+        }
+
         else{
             imageView.setImageResource(R.drawable.image);
             bitmap = ((BitmapDrawable) imageView.getDrawable()).getBitmap();
+            return bitmap;
         }
-
     }
 
     public void next(View v) {
